7e0c26a00a0241dd527a990629ba0039a994a6bc
Peter Barry <peter@everve.com>
2016-05-11 18:42:47 +0100

    Fix back/test:unit by updating DynamoDb calls

--- back/pot-service/handler.js
+++ back/pot-service/handler.js
@@ -1,43 +1,60 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-const uuid = require('node-uuid');
-const  HTTPError = require('node-http-error');
+const AWS       = require('aws-sdk');
+const uuid      = require('node-uuid');
+const HTTPError = require('node-http-error');
+const env       = require("../lib/environment.js");
 
-const env = require("../lib/environment.js");
-const db = require("../lib/dynamodb.js");
 
+const dynamoConfig = {
+    region: env.region
+}
+
+if (env.offline) dynamoConfig.endpoint = process.env.LOCAL_DDB_ENDPOINT;
+
+const db = new AWS.DynamoDB.DocumentClient({service: new AWS.DynamoDB(dynamoConfig)});
+
+console.log("Pot Handler environment loaded.");
 
 module.exports.handler = function (event, context) {
 
     switch (event.httpMethod) {
+
         case "GET":
-            var potId = event.pathParams['potId'];
-            if (potId) {
-                //Get individual pot:
-                dynamo.getItem({TableName: "Pot", Item: event.body}, function (err, data) {
-                    if (err) {
-                        context.done(JSON.stringify({errorCode: 500, reason: "Did not find pot:" + potId}));
-                    } else {
-                        context.done(null, data);
-                    }
-                });
-            }else{
-                context.done(JSON.stringify({errorCode: 500, reason: "We don't support listing all pots yet." }));
-            }
-            break;
+            // var potId = event.pathParams['potId'];
+            // if (potId) {
+            //     //Get individual pot:
+            //     dynamo.getItem({TableName: "Pot", Item: event.body}, function (err, data) {
+            //         if (err) {
+            //             context.done(JSON.stringify({errorCode: 500, reason: "Did not find pot:" + potId}));
+            //         } else {
+            //             context.done(null, data);
+            //         }
+            //     });
+            // }else{
+            //     context.done(JSON.stringify({errorCode: 500, reason: "We don't support listing all pots yet." }));
+            // }
+            // break;
 
         case "POST":
-            var key = uuid.v4();
-            var content = {
-                key: key,
-                user: event.json.userId,
-                raw: event.json
+            
+            var putPotMessage = {
+                    Item: {
+                        potId:  uuid.v4(), // create a new pot.
+                        userId: event.json.userId    
+                    }, 
+                    TableName: env.tables.pots
             };
-            db('put', {
-		                TableName: env.tables.pots,
-                        Item: content
-            }).then((res) => res);
+
+            db.put(putPotMessage,function(err, data) {
+		          if(err){
+                    console.log(err);
+                    context.done(JSON.stringify({errorCode: "Problem saving pot.", reason: "Your best guess."}));
+                  }else{
+                    console.log(data);
+                    context.done(null, JSON.stringify({data: "You saved something"}));
+                  }     
+            });
             break;
 
         case "PATCH":
