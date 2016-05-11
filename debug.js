6ad3ef8b5dbc7eb490f87b1bdc0cf5854fc3f03a
Peter Barry <peter@everve.com>
2016-05-12 00:36:51 +0100

    Create debug.js

---
+++ debug.js
@@ -0,0 +1,13 @@
+process.env.DEBUG = '*';
+
+var S = require('serverless');
+var serverless = new S({
+  projectPath: process.cwd(),
+  interactive: false
+});
+
+serverless.init().then(function() {
+  serverless.command({
+    _: ['offline', 'start']
+  });
+});
