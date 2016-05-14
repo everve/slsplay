9c52524787892c1bbcc3a9dd2a5419a9199b029e
Peter Barry <peter@everve.com>
2016-05-14 21:23:29 +0100

    travis

--- back/run-ci-int-test.sh
+++ back/run-ci-int-test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+bash --version
+serverless --version
 set -e
 pushd ..
 export CI=true
 sls project init -s travislocal -r eu-west-1  -p bogus -c -C
 sleep 5
-sls variables "set" -s travislocal -r eu-west-1 -k localDynamoDbEndpoint -v "http://localhost:8000"
+sls variables "set" -s "travislocal" -r "eu-west-1" -k "localDynamoDbEndpoint" -v "http://localhost:8000"
 
 sudo docker-compose up -d
 sls setup db -s travislocal -r eu-west-1
