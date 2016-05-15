ef82685944a4492f57a55c860310da515089ddda
Peter Barry <peter@everve.com>
2016-05-15 09:30:06 +0100

    travis

--- back/scripts/run-ci-int-test.sh
+++ back/scripts/run-ci-int-test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 bash --version
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
 set -v
-pushd ..
-pushd ..
+pushd "$DIR/.."
 export CI=true
 sls project init -s travislocal -r eu-west-1  -p bogus -c -C
 sleep 5
@@ -13,7 +13,7 @@ sls variables "set" -t "region" -s "travislocal" -r "eu-west-1" -k "localDynamoD
 sudo docker-compose up -d
 sls setup db -s travislocal -r eu-west-1
 popd
-node --debug server.js &
+node --debug scripts/server.js &
 SERVER_PID=$!
 sleep 5
 npm run-script test:integration
