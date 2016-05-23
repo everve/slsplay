c72fb749bfe96e86b812cfd84d4e46fc59aee61e
Peter Barry <peter@everve.com>
2016-05-23 20:18:19 +0100

    fix tests

--- back/tests/schema.validator.spec.js
+++ back/tests/schema.validator.spec.js
@@ -8,19 +8,8 @@ const Ajv = require("ajv");
 var ajv = Ajv([]);
 
 test('Check meetup schemas validate against samples.', function (t) {
-    t.plan(2); //annoying find nicer way.
-    schemaLoader.forEachJsonSchemaPair(__dirname + "/../meetup-service/schema",  (schema, json)=>{
-        result = ajv.validate(JSON.parse(schema.content), JSON.parse(json.content));
-        console.log(schema.name + "<=>" + json.name);
-        console.log(ajv.errors);
-        t.equal(result, true);
-
-    });
-});
-
-test('Check invitees schemas validate against samples.', function (t) {
     t.plan(1); //annoying find nicer way.
-    schemaLoader.forEachJsonSchemaPair(__dirname + "/../invitee-service/schema",  (schema, json)=>{
+    schemaLoader.forEachJsonSchemaPair(__dirname + "/../meetup-service/schema",  (schema, json)=>{
         result = ajv.validate(JSON.parse(schema.content), JSON.parse(json.content));
         console.log(schema.name + "<=>" + json.name);
         console.log(ajv.errors);
